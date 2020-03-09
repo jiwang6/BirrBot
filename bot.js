db0ce7ea01e7179bdebc019f53df2ea97936651b
jiwang6 <jim.z.wang8@gmail.com>
2020-03-08 21:05:31 -0600

    time test pt 2

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ function respond() {
   // (request.text && botRegex.test(request.text))
   // weekday response
   // if (date.getHours() == 20 && (date.getDay != 5 || date.getDay != 6))
-  if (date.getHours() == 21 && (date.getDay != 5 || date.getDay != 6)) { 
+  if ((request.text && botRegex.test(request.text)) || date.getHours() == 21 && date.getMinutes == 10) { 
     this.res.writeHead(200);
     postMessage();
     this.res.end();
