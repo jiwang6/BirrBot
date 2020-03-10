bbfa08615e6f8f31121e7e2c70f01fc4327699a6
jiwang6 <jim.z.wang8@gmail.com>
2020-03-09 19:05:25 -0600

    cross-chat double messaging

--- script.js
+++ script.js
@@ -42,5 +42,43 @@ function postMessage() {
   botReq.end(JSON.stringify(body));
 }
 
+function postMessage2() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'like for boxed lunches';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : '5fdff496ef5e210bb66befa88c', // change to LunchBot botID after test tonight
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        // neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+if (date.getDay !=5 || date.getDay!=6) {
+  postMessage2();
+}
 
 postMessage();
---
+++ script2.js
@@ -0,0 +1,48 @@
+// 5fdff496ef5e210bb66befa88c
+
+var HTTPS = require('https');
+
+// var botID = process.env.BOT_ID;
+
+var botID = '5fdff496ef5e210bb66befa88c';
+
+var date = new Date();
+
+function postMessage() {
+  var botResponse, options, body, botReq;
+
+  botResponse = 'like for boxed lunches';
+
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : '5fdff496ef5e210bb66befa88c',
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
+
+if (date.getDay !=5 || date.getDay!=6) {
+  postMessage();
+}
