7c5b15af854efceebf76c272c7f8fbbbc07fa65c
Jim Wang <54686917+jiwang6@users.noreply.github.com>
2020-11-10 11:12:39 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var date = new Date();
 
 function respond() {
   var request = JSON.parse(this.req.chunks[0]),
-      botRegex = /^\/joke$/;
+      botRegex = /^\/ahaha$/;
 
   if (request.text && botRegex.test(request.text)) { 
     this.res.writeHead(200);
@@ -24,7 +24,7 @@ function respond() {
 function postMessage() {
   var botResponse, options, body, botReq;
 
-  botResponse = 'https://drive.google.com/file/d/1ParNegKTHfQFbgfcBWuw-ZwT6vtsVyMt/view?usp=sharing';
+  botResponse = newJoke;
 
   options = {
     hostname: 'api.groupme.com',
@@ -54,6 +54,13 @@ function postMessage() {
     console.log('timeout posting message '  + JSON.stringify(err));
   });
   botReq.end(JSON.stringify(body));
+}
+
+var jokes= ['Guess the number of programmers it takes to change a light bulb? Zero its a hardware problem','There are only 10 kinds of people in this world: those who know binary and those who don’t.','Real programmers count from 0.', 'Why did the programmer quit his job? Because he didnt get arrays.', 'A foo walks into a bar takes a look around and says Hello World','0 is false 1 is true right? 1','Things arent always #000000 and #FFFFFF.','What is the most used language in programming? Profanity','!False its funny because its True','You had me at Hello World','2b||!2b','Yesterday I changed the name on my wifi to Hack if you can. Today I found it named Challenge Accepted','A programmer is a person who fixed a problem that you didnt know you had in a way you dont understand','How can you tell if a computer geek is an extrovert? They stare at your shoes when you talk instead of their own.','I would love to change the world but they wont give me the source code.','If at first you dont succedd call it version 1.0','Computers make very fast very accurate mistakes','I farted in the Apple store and everyone got mad at me. Not my fault they dont have Windows.','Knock Knock... Whos there? Art... Art Who? R2D2','Hilarious and amazingly true thing: if a pizza has a radius (z) and a depth (a) that pizzas volume can be defined Pi*z*z*a.']
+
+function newJoke(){
+  var randomNumber=Math.floor(Math.random()*(20));
+  return jokes[randomNumber];
 }
 
 
